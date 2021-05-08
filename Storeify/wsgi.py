8c978520cb4b9309a2a592769cc01ced0bdb1291
Ibby360 <ibrahimramadhan360@outlook.com>
2021-05-09 00:41:40 +0300

    Intial commit

---
+++ Storeify/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Storeify project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Storeify.settings')
+
+application = get_wsgi_application()
