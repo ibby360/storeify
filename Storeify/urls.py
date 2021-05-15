0fc40d17459a2e10708d4cf6d8b5acfc3be21883
Ibby360 <ibrahimramadhan360@outlook.com>
2021-05-16 02:37:00 +0300

    Add home page url path

--- Storeify/urls.py
+++ Storeify/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from mainApp import views
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', views.index, name='index'),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
