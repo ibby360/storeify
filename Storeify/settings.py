89dc2529582e9bd41fe22bb3348ea4c5f69b949c
Ibby360 <ibrahimramadhan360@outlook.com>
2021-05-20 23:10:05 +0300

    Add path to the root directory where the files are getting stored

--- Storeify/settings.py
+++ Storeify/settings.py
@@ -128,6 +128,10 @@ STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 #Location of static files
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
+# Media Files Configurations
+MEDIA_URL = '/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
