8f8738c227828cc3a1b4060b1c179f55273eda72
Ibby360 <ibrahimramadhan360@outlook.com>
2021-05-20 23:09:23 +0300

    Servimg media files in development

--- Storeify/urls.py
+++ Storeify/urls.py
@@ -24,3 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
