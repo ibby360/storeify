a0c48004f938e4a843de2745f48461d532fb7564
Ibby360 <ibrahimramadhan360@outlook.com>
2021-06-05 02:18:13 +0300

    Product model

---
+++ shop/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from category.models import Category
+
+# Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=150)
+    slug = models.SlugField(unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True)
+    thumbnail = models.ImageField(upload_to='products')
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=False)
+    trending_product = models.BooleanField(default=False)
+    top_seller = models.BooleanField(default=False)
+    featured = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        return self.product_name
