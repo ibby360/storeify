f93b50eaa88a26ae975d7c13667e70a4584f552c
Ibby360 <ibrahimramadhan360@outlook.com>
2021-06-05 02:15:25 +0300

    Display products at the home pahe

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from shop.models import Product
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
+    trending_products = Product.objects.filter(trending_product=True, is_available=True)
+    top_sellers = Product.objects.filter(top_seller=True, is_available=True)
+    featured = Product.objects.filter(featured=True, is_available=True)[:3]
+
+    context = {
+        'trending_products': trending_products,
+        'top_sellers': top_sellers,
+        'featured': featured
+    }
+    return render(request, 'index.html', context)
