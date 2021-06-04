a7dd35074a20ece4c096b937a77238b8e780f409
Ibby360 <ibrahimramadhan360@outlook.com>
2021-06-05 02:16:53 +0300

    Add shop app and category context_processors

--- Storeify/settings.py
+++ Storeify/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     # My Apps
     'mainApp',
     'category',
+    'shop',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'category.context_processors.cat_links',
             ],
         },
     },
