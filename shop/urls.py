2d3f9010db1965534217351f766ee4ae7fce464d
Ibby360 <ibrahimramadhan360@outlook.com>
2021-06-13 16:58:20 +0300

    get single product with category_slug and product_slug

--- shop/urls.py
+++ shop/urls.py
@@ -5,4 +5,5 @@ app_name = 'shop'
 urlpatterns = [
     path('', views.shop, name='shop'),
     path('<slug:category_slug>/', views.shop, name='product_by_category'),
+    path('<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
 ]
