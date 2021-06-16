fc4002d604c39f5004e0945fb9ada577e7f361ff
Ibby360 <ibrahimramadhan360@outlook.com>
2021-06-16 16:51:01 +0300

    Add cart URLConf

--- Storeify/urls.py
+++ Storeify/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('shop/', include('shop.urls')),
+    path('cart/', include('cart.urls')),
 
 
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
