d6c9b445b1a9468068195af05ff1524caf2374a3
Ibby360 <ibrahimramadhan360@outlook.com>
2021-06-16 16:53:11 +0300

    Check if product is already added to the cart

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from shop.models import Product
 from category.models import Category
+from cart.models import CartItem
+from cart.views import _cart_id
 
 # Create your views here.
 
@@ -24,10 +26,13 @@ def shop(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request),product=product)
+
     except Exception as e:
         raise e
     
     context = {
-        'product': product
+        'product': product,
+        'in_cart': in_cart
     }
     return render(request, 'shop/single-product.html', context)
