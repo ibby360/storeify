ca2beeb8a9fbdbc2d69090f2538160b5998ca1a1
Ibby360 <ibrahimramadhan360@outlook.com>
2021-06-16 16:54:28 +0300

    Cart app UrlConf

---
+++ cart/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from cart import views
+
+app_name = 'cart'
+
+urlpatterns = [
+    path('', views.cart, name='cart'),
+    path('add_cart/<int:product_id>/', views.add_cart, name='add_cart'),
+    path('remove_cart/<int:product_id>/', views.cat_remove, name='remove_cart'),
+    path('remove_cart_item/<int:product_id>/', views.cat_remove_item, name='remove_cart_item'),
+]
+
