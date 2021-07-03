7632f6933e85be119c893518dec23da65c9dc0d9
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-04 00:28:16 +0300

    Check if password and confirm password fields matches

--- accounts/forms.py
+++ accounts/forms.py
@@ -15,6 +15,15 @@ class RegistrationForm(forms.ModelForm):
         model = Account
         fields = ['username', 'phone_number', 'email', 'password']
 
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password does not match!"
+            )
+
 
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
