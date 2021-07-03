5659e58fb02487b00e23c86fc033399cc0eb7ab1
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-04 00:31:44 +0300

    Create AccountAdmin class

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from accounts.models import Account
 
 # Register your models here.
+
+class AccountAdmin(UserAdmin):
+    list_display = ('username', 'email', 'last_login', 'date_joined', 'is_active')
+    list_display_links = ('email', 'username')
+    readonly_fields = ('last_login', 'date_joined')
+    ordering = ('-date_joined',)
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Account, AccountAdmin)
