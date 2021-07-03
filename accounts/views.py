023a504af49c6e484c8e9d0141147fd557ac4b65
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-04 01:47:38 +0300

    handle user login and logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from accounts.forms import RegistrationForm
 from accounts.models import Account
+from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
 
 def register(request):
     if request.method == 'POST':
@@ -12,7 +15,8 @@ def register(request):
             phone_number = form.cleaned_data['phone_number']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
-            user = Account.objects.create_user(username=username, email=email, password=password)
+            user = Account.objects.create_user(
+                username=username, email=email, password=password)
             user.phone_number = phone_number
             user.save()
     else:
@@ -25,4 +29,21 @@ def register(request):
 
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            # messages.error('Invalid login credentials!')
+            return redirect('login')
     return render(request, 'accounts/login.html')
+
+@login_required(login_url='login')
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
