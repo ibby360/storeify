7700228ef03e5d0003810bdcb92b68f823c63fb4
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-16 22:56:56 +0300

    User email activation

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,16 @@ from accounts.models import Account
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 
+
+# Email Verification
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
+
 # Create your views here.
 
 
@@ -19,6 +29,19 @@ def register(request):
                 username=username, email=email, password=password)
             user.phone_number = phone_number
             user.save()
+
+            # USER ACTIVATION
+            current_site = get_current_site(request)
+            mail_subject = 'Welcome to VirtTips! Confirm Your Email.'
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
     else:
         form = RegistrationForm()
 
