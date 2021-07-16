c3003ab08f22c5b14f6e84122c33049e8cc2ff23
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-17 00:26:36 +0300

    user email activation URL

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 
 ]
