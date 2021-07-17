708018c0f789a1847592859606fd76b6ad03e320
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-17 16:30:14 +0300

    Custom user model for authentication

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,26 +8,27 @@ class RegistrationForm(forms.ModelForm):
         'class': 'form-control',
     }))
     confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Confirm Password'           
+        'placeholder': 'Confirm Password'
     }))
 
     class Meta:
         model = Account
-        fields = ['username', 'phone_number', 'email', 'password']
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
 
     def clean(self):
         cleaned_data = super(RegistrationForm, self).clean()
         password = cleaned_data.get('password')
         confirm_password = cleaned_data.get('confirm_password')
+
         if password != confirm_password:
             raise forms.ValidationError(
                 "Password does not match!"
             )
 
-
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter last Name'
         self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
         self.fields['email'].widget.attrs['placeholder'] = 'Enter Email Address'
         for field in self.fields:
