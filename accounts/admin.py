35fe27b05e40bbd707c544110c8dd0bf9be988b7
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-17 16:30:07 +0300

    Custom user model for authentication

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from accounts.models import Account
+from .models import Account
 
 # Register your models here.
 
 class AccountAdmin(UserAdmin):
-    list_display = ('username', 'email', 'last_login', 'date_joined', 'is_active')
-    list_display_links = ('email', 'username')
+    list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active')
+    list_display_links = ('email', 'first_name', 'last_name')
     readonly_fields = ('last_login', 'date_joined')
     ordering = ('-date_joined',)
 
