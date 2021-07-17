00cbb57b3961d494247ebc07117f53439d7ba024
Ibby360 <ibrahimramadhan360@outlook.com>
2021-07-17 16:29:53 +0300

    Custom user model for authentication

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,11 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 # Create your models here.
 
 class MyAccountManager(BaseUserManager):
-    def create_user(self,  username, email, password=None):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+    def create_user(self, first_name, last_name, username, email, password=None):
         if not email:
             raise ValueError('User must have an email address')
 
@@ -13,19 +17,26 @@ class MyAccountManager(BaseUserManager):
             raise ValueError('User must have an username')
 
         user = self.model(
-            email=self.normalize_email(email),
-            username=username,
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
         )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, username, password):
+    def create_superuser(self, first_name, last_name, email, username, password):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
         user = self.create_user(
-            email=self.normalize_email(email),
-            username=username,
-            password=password,
+            email = self.normalize_email(email),
+            username = username,
+            password = password,
+            first_name = first_name,
+            last_name = last_name,
         )
         user.is_admin = True
         user.is_active = True
@@ -35,26 +46,29 @@ class MyAccountManager(BaseUserManager):
         return user
 
 
+
 class Account(AbstractBaseUser):
-    username = models.CharField(max_length=50, unique=True, null=False, blank=False)
-    email = models.EmailField(max_length=100, unique=True)
-    phone_number = models.CharField(max_length=50)
+    first_name      = models.CharField(max_length=50)
+    last_name       = models.CharField(max_length=50)
+    username        = models.CharField(max_length=50, unique=True)
+    email           = models.EmailField(max_length=100, unique=True)
+    phone_number    = models.CharField(max_length=50)
 
     # required
-    date_joined = models.DateTimeField(auto_now_add=True)
-    last_login = models.DateTimeField(auto_now_add=True)
-    is_admin = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
-    is_superadmin = models.BooleanField(default=False)
+    date_joined     = models.DateTimeField(auto_now_add=True)
+    last_login      = models.DateTimeField(auto_now_add=True)
+    is_admin        = models.BooleanField(default=False)
+    is_staff        = models.BooleanField(default=False)
+    is_active        = models.BooleanField(default=False)
+    is_superadmin        = models.BooleanField(default=False)
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['username', 'email', 'phone_number']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     objects = MyAccountManager()
 
-    def __str__(self):
-        return self.username
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
 
     def __str__(self):
         return self.email
