aee8cbc1f65dfe00205fdf408c1c1fb1cbb1e618
Ibby360 <ibrahimramadhan360@outlook.com>
2022-01-18 22:16:10 +0300

    Include tax

--- cart/views.py
+++ cart/views.py
@@ -120,18 +120,27 @@ def cart(request, total=0, quantity=0, cart_items=None):
 def checkout(request, total=0, quantity=0, cart_items=None):
     auth = login(request)
     try:
-        cart = Cart.objects.get(cart_id=session_cart_id(request))
-        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        tax = 0
+        grand_total = 0
+        if request.user.is_authenticated:
+           cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+        else:
+            cart = Cart.objects.get(cart_id=session_cart_id(request))
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
 
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+        tax = (2 * total) / 100
+        grand_total = total + tax
     except ObjectDoesNotExist:
         pass
 
     context = {
         'auth': auth,
         'total': total,
+        'tax': tax,
+        'grand_total': grand_total,
         'quantity': quantity,
         'cart_items': cart_items,
     }
